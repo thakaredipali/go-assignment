b1dab92c2e34b7f85ec5850c5d87dfd5fac3f7d3
Dipali Thakare <dipalithakare96@gmail.com>
2024-09-11 18:00:38 +0530

    Completed assignment 8

---
+++ assignment8/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func isEven(n int) bool {
+	return n%2 == 0
+}
+
+func main() {
+	n := 0
+
+	res := make(chan bool)
+	go func() {
+
+		nIsEven := isEven(n)
+		time.Sleep(5 * time.Millisecond)
+		if nIsEven {
+			fmt.Println(n, " is even")
+			res <- true
+			return
+		}
+		fmt.Println(n, "is odd")
+
+	}()
+	<-res
+	go func() {
+		n++
+	}()
+
+	time.Sleep(time.Second)
+}
